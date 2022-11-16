266a83137a958b3f1fa73848de065a18007ed8e0
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2022-11-16 15:01:26 -0500

    works with 4 modules

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,5 +1,5 @@
 use self::ctrl_state::CtrlState;
-use crate::{symbols::symbol_cache, to_bits::ToBits};
+use crate::symbols::symbol_cache;
 use anyhow::{anyhow, Ok, Result};
 use embedded_hal::digital::v2::OutputPin;
 use std::{thread::sleep, time::Duration};
@@ -25,6 +25,14 @@ pub enum Command {
     DisplayTest = 0x0F,
 }
 
+#[derive(Copy, Clone)]
+pub enum DecodeMode {
+    NoDecode = 0x00,
+    CodeBDigit0 = 0x01,
+    CodeBDigits3_0 = 0x0F,
+    CodeBDigits7_0 = 0xFF,
+}
+
 pub struct MaxController<DATA, CS, CLK>
 where
     DATA: OutputPin,
@@ -32,137 +40,80 @@ where
     CLK: OutputPin,
 {
     module_count: u8,
-    // connection: MAX7219<PinConnector<DATA, CS, CLK>>,
     state: CtrlState,
     data: DATA,
     cs: CS,
     clk: CLK,
-    buffer: [u8; 8 * 2],
+    buffer: Vec<u8>,
 }
 
+const MAX_DIGITS: usize = 8;
+
+/// controller functions
 impl<DATA, CS, CLK> MaxController<DATA, CS, CLK>
 where
     DATA: OutputPin,
     CS: OutputPin,
     CLK: OutputPin,
 {
-    pub fn new(
-        module_count: u8,
-        data: DATA,
-        cs: CS,
-        clk: CLK,
-    ) -> Result<MaxController<DATA, CS, CLK>> {
-        println!("{}", module_count);
-        Ok(MaxController {
+}
+
+// initialization functions
+impl<DATA, CS, CLK> MaxController<DATA, CS, CLK>
+where
+    DATA: OutputPin,
+    CS: OutputPin,
+    CLK: OutputPin,
+{
+    pub fn new(module_count: u8, data: DATA, cs: CS, clk: CLK) -> Result<Self> {
+        let mut ret_val = MaxController {
             module_count,
             state: ctrl_state::CtrlState::new(module_count),
             data,
             cs,
             clk,
-            buffer: [0; 8 * 2],
-        })
+            buffer: vec![0; 8 * 2],
+        };
+
+        ret_val.init()?;
+
+        Ok(ret_val)
     }
 
-    pub fn init(&mut self) -> Result<()> {
+    fn init(&mut self) -> Result<()> {
         for i in 0..self.module_count {
-            self.test_mode(i, false)?;
-            self.send_command(i, Command::ScanLimit, 0x07)?;
-            self.send_command(i, Command::DecodeMode, 0)?;
-            self.clear(i)?;
+            self.set_test_mode(i, false)?;
+            self.write_raw(i, Command::ScanLimit as u8, 7)?;
+            self.write_raw(i, Command::DecodeMode as u8, 0)?;
+            self.clear_display(i)?; // clear all digits
+        }
+        self.power_off()?; // power off
+
+        Ok(())
+    }
+}
+
+/// write functions
+impl<DATA, CS, CLK> MaxController<DATA, CS, CLK>
+where
+    DATA: OutputPin,
+    CS: OutputPin,
+    CLK: OutputPin,
+{
+    pub fn write(&mut self, addr: u8, raw: &[u8; MAX_DIGITS]) -> Result<()> {
+        let mut digit: u8 = 0;
+        for b in raw {
+            self.write_raw(addr, digit % 8 + 1, *b)?;
+            digit += 1;
         }
         Ok(())
     }
 
-    pub fn clear(&mut self, addr: u8) -> Result<()> {
-        for i in 1..9 {
-            self.write_raw(addr as usize, i, 0x00)?;
-        }
-
-        Ok(())
-    }
-
-    pub fn test_mode(&mut self, addr: u8, is_on: bool) -> Result<()> {
-        if is_on {
-            self.send_command(addr, Command::DisplayTest, 0x01)
-        } else {
-            self.send_command(addr, Command::DisplayTest, 0x00)
-        }
-    }
-
-    pub fn power_on(&mut self) {
-        for i in 0..self.module_count {
-            self.write_raw(i as usize, Command::Power as u8, 0x01)
-                .unwrap();
-        }
-    }
-
-    pub fn power_on_at(&mut self, addr: u8) {
-        self.send_command(addr, Command::Power, 1);
-    }
-
-    pub fn power_off(&mut self) {
-        for i in 0..self.module_count {
-            self.send_command(i, Command::Power, 0);
-        }
-    }
-
-    pub fn power_off_at(&mut self, addr: u8) {
-        self.send_command(addr, Command::Power, 0);
-    }
-
-    pub fn send_command(&mut self, addr: u8, command: Command, data: u8) -> Result<()> {
-        self.write_raw(addr as usize, command as u8, data)
-
-        // for board_n in 0..self.module_count {
-        //     self.cs
-        //         .set_low()
-        //         .map_err(|_| anyhow!(format!("{}:{}", file!(), line!())))?;
-        //     if board_n != addr {
-        //         self.send_bits(0u8.to_bits())
-        //             .map_err(|_| anyhow!(format!("{}:{}", file!(), line!())))?;
-        //         self.send_bits(0u8.to_bits())
-        //             .map_err(|_| anyhow!(format!("{}:{}", file!(), line!())))?;
-        //     } else {
-        //         self.send_bits((command as u8).to_bits())
-        //             .map_err(|_| anyhow!(format!("{}:{}", file!(), line!())))?;
-        //         self.send_bits((data as u8).to_bits())
-        //             .map_err(|_| anyhow!(format!("{}:{}", file!(), line!())))?;
-        //     }
-        //     self.cs
-        //         .set_high()
-        //         .map_err(|_| anyhow!(format!("{}:{}", file!(), line!())))?;
-
-        // }
-        // Ok(())
-    }
-
-    // pub fn write_raw(&mut self, addr: u8, bits: &[u8; 8]) -> Result<()> {
-    //     for row in 0..8u8 {
-    //         for board_n in 0..self.module_count {
-    //             self.cs
-    //                 .set_low()
-    //                 .map_err(|_| anyhow!(format!("{}:{}", file!(), line!())))?;
-    //             if board_n != addr {
-    //                 self.send_bits(0u16.to_bits())
-    //                     .map_err(|_| anyhow!(format!("{}:{}", file!(), line!())))?;
-    //             } else {
-    //                 self.send_bits(row.to_bits())
-    //                     .map_err(|_| anyhow!(format!("{}:{}", file!(), line!())))?;
-    //                 self.send_bits(bits[row as usize].to_bits())
-    //                     .map_err(|_| anyhow!(format!("{}:{}", file!(), line!())))?;
-    //             }
-    //             self.cs
-    //                 .set_high()
-    //                 .map_err(|_| anyhow!(format!("{}:{}", file!(), line!())))?;
-    //         }
-    //     }
-    //     Ok(())
-    // }
-
-    fn write_raw(&mut self, addr: usize, header: u8, data: u8) -> Result<()> {
-        let offset = addr * 2;
-        let max_bytes = self.module_count * 2;
-        self.buffer = [0; 8 * 2];
+    pub fn write_raw(&mut self, addr: u8, header: u8, data: u8) -> Result<()> {
+        println!("addr {}\theader {}\tdata {}", addr, header, data);
+        let offset = addr as usize * 2;
+        let max_bytes = self.module_count as usize * 2;
+        self.buffer = vec![0; max_bytes];
 
         self.buffer[offset] = header;
         self.buffer[offset + 1] = data;
@@ -171,10 +122,10 @@ where
             .set_low()
             .map_err(|_| anyhow!(format!("{}:{}", file!(), line!())))?;
         for b in 0..max_bytes {
-            let value = self.buffer[b as usize];
+            let value = self.buffer[b];
 
             for i in 0..8 {
-                if value & (1 << (7 - i)) > 0 {
+                if value & (1 << (7 - i)) != 0 {
                     self.data
                         .set_high()
                         .map_err(|_| anyhow!(format!("{}:{}", file!(), line!())))?;
@@ -198,78 +149,113 @@ where
 
         Ok(())
     }
+}
 
-    fn send_bits(&mut self, bits: Vec<bool>) -> Result<()> {
-        for b in bits {
-            match b {
-                true => self
-                    .data
-                    .set_high()
-                    .map_err(|_| anyhow!(format!("{}:{}", file!(), line!())))?,
-                false => self
-                    .data
-                    .set_low()
-                    .map_err(|_| anyhow!(format!("{}:{}", file!(), line!())))?,
+/// test functions
+impl<DATA, CS, CLK> MaxController<DATA, CS, CLK>
+where
+    DATA: OutputPin,
+    CS: OutputPin,
+    CLK: OutputPin,
+{
+    ///
+    pub fn test_letter(&mut self) -> Result<()> {
+        for (i, symbol) in ('A'..='Z').enumerate() {
+            if symbol_cache::is_available_symbol(&symbol) {
+                self.write(
+                    i as u8 % self.module_count,
+                    symbol_cache::get_symbol(&symbol).unwrap().get_u8_grid(),
+                )?;
+                sleep(Duration::from_millis(700));
             }
-            self.clk
-                .set_high()
-                .map_err(|_| anyhow!(format!("{}:{}", file!(), line!())))?;
-            self.clk
-                .set_low()
-                .map_err(|_| anyhow!(format!("{}:{}", file!(), line!())))?;
-        }
-        Ok(())
-    }
-
-    pub fn test(&mut self, sleep_time: Duration) -> anyhow::Result<()> {
-        for mod_addr in 0..self.module_count {
-            self.test_mode(mod_addr, true)
-                .map_err(|_| anyhow!(format!("{}:{}", file!(), line!())))?;
-
-            sleep(sleep_time);
-
-            self.test_mode(mod_addr, false)
-                .map_err(|_| anyhow!(format!("{}:{}", file!(), line!())))?;
         }
         Ok(())
     }
 
     pub fn test_symbol(&mut self) {
-        let mut symbol_src = symbol_cache::cache_keys();
-        symbol_src.sort();
-
-        let mut symbol_vec = vec![symbol_src.iter().collect::<Vec<_>>()];
-
-        for i in 1..self.module_count as usize {
-            symbol_vec.push(
-                symbol_src
-                    .iter()
-                    .skip(i)
-                    .chain(symbol_src.iter().take(i))
-                    .collect(),
-            );
-        }
-
-        println!("{:#?}", symbol_vec);
-
-        for o in 0..symbol_src.len() {
-            for i in 0..self.module_count {
-                println!("sending {} to {}", symbol_vec[i as usize][o], i);
-                if let Err(e) = self.write_raw(
-                    i as usize,
-                    1,
-                    symbol_cache::get_symbol(symbol_vec[i as usize][o])
-                        .unwrap()
-                        .get_u8_grid()[0],
+        let mut symbol_vec = symbol_cache::cache_keys();
+        symbol_vec.sort();
+        for symbols in symbol_vec
+            .iter()
+            .zip(symbol_vec.iter().skip(1).chain(symbol_vec.iter().take(1)))
+            .zip(symbol_vec.iter().skip(2).chain(symbol_vec.iter().take(2)))
+            .zip(symbol_vec.iter().skip(3).chain(symbol_vec.iter().take(3)))
+            .map(|(((symbol_0, symbol_1), symbol_2), symbol_3)| {
+                vec![symbol_0, symbol_1, symbol_2, symbol_3]
+            })
+        {
+            for (i, symbol) in symbols.iter().enumerate() {
+                if let Err(e) = self.write(
+                    i as u8,
+                    symbol_cache::get_symbol(symbol).unwrap().get_u8_grid(),
                 ) {
-                    eprintln!(
-                        "error in getting u8 grid of {}\n error: {:?}",
-                        symbol_vec[i as usize][o], e
-                    );
+                    eprintln!("error in getting u8 grid of {}\n error: {:?}", symbol, e);
                 }
             }
-            println!("sleeping");
             sleep(Duration::from_millis(500));
+        }
+    }
+
+    /// set each segment to test mode 1 by 1
+    pub fn run_test(&mut self, sleep_time: Duration) -> anyhow::Result<()> {
+        for mod_addr in 0..self.module_count {
+            self.set_test_mode(mod_addr, true)?;
+
+            sleep(sleep_time);
+
+            self.set_test_mode(mod_addr, false)?;
+        }
+        Ok(())
+    }
+}
+
+/// max7219 commands
+impl<DATA, CS, CLK> MaxController<DATA, CS, CLK>
+where
+    DATA: OutputPin,
+    CS: OutputPin,
+    CLK: OutputPin,
+{
+    pub fn clear(&mut self) -> Result<()> {
+        for addr in 0..self.module_count {
+            for i in 1..9 {
+                self.write_raw(addr, i, 0)?;
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn clear_display(&mut self, addr: u8) -> Result<()> {
+        for i in 1..9 {
+            self.write_raw(addr, i, 0)?;
+        }
+
+        Ok(())
+    }
+    pub fn set_intensity(&mut self, addr: u8, intensity: u8) -> Result<()> {
+        self.write_raw(addr, Command::Intensity as u8, intensity)
+    }
+
+    pub fn power_on(&mut self) -> Result<()> {
+        for i in 0..self.module_count {
+            self.write_raw(i, Command::Power as u8, 1)?;
+        }
+        Ok(())
+    }
+
+    pub fn power_off(&mut self) -> Result<()> {
+        for i in 0..self.module_count {
+            self.write_raw(i, Command::Power as u8, 0)?;
+        }
+        Ok(())
+    }
+
+    pub fn set_test_mode(&mut self, addr: u8, is_on: bool) -> Result<()> {
+        if is_on {
+            self.write_raw(addr, Command::DisplayTest as u8, 0x01)
+        } else {
+            self.write_raw(addr, Command::DisplayTest as u8, 0x00)
         }
     }
 
